7cb24b0c75da6181e5c845f18052c1b51d113838
jurgen5715 <guevarajurgen8@gmail.com>
2026-09-16 20:39:26 -0600

    Semana 2

---
+++ Portafolio_Jurgen/src/test/java/Portafolio_Jurgen/PortafolioApplicationTests.java
@@ -0,0 +1,13 @@
+package Portafolio_Jurgen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortafolioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
